6f93a7d9ab6fe06c37fe07db485ac9765f72228e
Daveonwave <davide.salaorni@mail.polimi.it>
2018-04-09 19:34:30 +0200

    First commit 2.0

---
+++ src/main/java/ingsw2018/App.java
@@ -0,0 +1,9 @@
+package ingsw2018;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Applicazione
+    }
+}
---
+++ src/test/java/ingsw2018/AppTest.java
@@ -0,0 +1,9 @@
+package ingsw2018;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    //Testing applicazione
+
+}
