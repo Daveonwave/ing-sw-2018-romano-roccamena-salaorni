d0a4012f5a6107d22ee0c5feeffd30befaec83be
MassimilianoRoccamena <massimiliano.roccamena@mail.polimi.it>
2018-04-16 14:07:15 +0200

    version 1.0 - sockets service temp

---
+++ src/main/java/service/socket/SocketServer.java
@@ -0,0 +1,63 @@
+package service.socket;
+
+import service.ServiceException;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public abstract class SocketServer<E extends Serializable, I extends Serializable, O extends Serializable>{
+    //Generico server socket astratto di servizi
+
+    private ServerSocket serverSocket;
+    private ExecutorService executorService;
+    private int port;
+
+    //Costruttore
+    public SocketServer(int port) {
+        this.serverSocket = null;
+        this.executorService = Executors.newCachedThreadPool();
+        this.port = port;
+    }
+
+    //Setter/Getter
+    protected void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    //Connessione
+    public void start() throws ServiceException {
+        try {
+            //Avvia server
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new ServiceException("can't start server");
+        }
+    }
+    public void close() throws ServiceException {
+        try {
+            //Chiude server
+            serverSocket.close();
+            serverSocket = null;
+        } catch (IOException e) {
+            throw new ServiceException("failed closing server");
+        }
+    }
+    public void accept(SocketController socketController) throws ServiceException {
+        try {
+            //Accetta un client
+            socketController.setSocket(serverSocket.accept());
+            //Avvia controllo
+            executorService.submit(socketController);
+        } catch (IOException e) {
+            throw new ServiceException("failed accepting connection and starting socket controller");
+        }
+    }
+}
---
+++ src/main/java/service/socket/SocketUser.java
@@ -0,0 +1,94 @@
+package service.socket;
+
+import service.ServiceException;
+import service.ServiceNotifier;
+import service.SelfSyncHandler;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+
+public abstract class SocketUser<E extends Serializable, I extends Serializable, O extends Serializable> extends SelfSyncHandler<E, I, O> implements ServiceNotifier<E, I, O> {
+    //Generico utilizzatore astratto di servizi che esegue una richiesta attraverso socket al controllore
+
+    private Socket socket;
+    private ObjectInputStream inStream;
+    private ObjectOutputStream outStream;
+    private String ip;
+    private int port;
+
+    //Costruttori
+    public SocketUser(String ip, int port) throws ServiceException {
+        this.socket = null;
+        this.inStream = null;
+        this.outStream = null;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    //Setter/Getter
+    protected void setIp(String ip) {
+        this.ip = ip;
+    }
+    protected void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+    public int getPort() {
+        return port;
+    }
+
+    //Connessione
+    public void connect() throws ServiceException {
+        try {
+            //Apre connessione
+            socket = new Socket(ip, port);
+
+            //Salva stream
+            inStream = new ObjectInputStream(socket.getInputStream());
+            outStream = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new ServiceException("can't connect with server");
+        }
+    }
+    public void disconnect() throws ServiceException {
+        try {
+            //Chiude gli stream
+            inStream.close();
+            inStream = null;
+            outStream.close();
+            outStream = null;
+
+            //Chiude connessione
+            socket.close();
+            socket = null;
+        } catch (IOException e) {
+            throw new ServiceException("can't disconnect with server");
+        }
+
+    }
+
+    //Notify
+    public O notifyRequest(E event, I input) throws ServiceException {
+        return raiseTask(event, input);
+    }
+    protected O handleTask(E event, I input) throws ServiceException {
+        try {
+            outStream.writeObject(event);
+            outStream.flush();
+            outStream.writeObject(input);
+            outStream.flush();
+
+            return (O) inStream.readObject();
+        } catch (IOException e) {
+            throw new ServiceException("failed writing to server");
+        } catch (ClassNotFoundException e) {
+            throw new ServiceException("failed reading from server");
+        }
+    }
+}
