ac21c6c2411d28041b0e893f4afe0fd3e128c234
Daveonwave <davide.salaorni@mail.polimi.it>
2018-07-01 17:04:18 +0200

    add javadoc

--- src/main/java/connection/rmi/RmiController.java
+++ src/main/java/connection/rmi/RmiController.java
@@ -12,6 +12,11 @@ import java.rmi.server.UnicastRemoteObject;
 public class RmiController extends AppController {
     //Implementazione RMI del controller
 
+    /**
+     * Class constructor
+     * @param timerConfig receives the timers that control the times of action during the match
+     * @throws RemoteException
+     */
     public RmiController(TimerConfig timerConfig) throws RemoteException {
         super(timerConfig);
 
